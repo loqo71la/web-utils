b94363ae1c55a7c96e1d602417790b7a322d93ab
Dan Canqui <dan_joca@hotmail.com>
2022-07-05 11:25:25 -0400

    Remove unnecessary items

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,14 +5,14 @@ const Home: NextPage = () => {
   return (
     <div>
       <Head>
-        <title>loqo71la's Web Utils</title>
+        <title>loqo71la&apos;s Web Utils</title>
         <meta name="description" content="Web Utils for loqo71la.dev" />
         <link rel="icon" href="/favicon.png" />
       </Head>
 
       <main>
         <h1>
-          Welcome to <a href="https://loqo71la.dev">loqo71la</a>'s Web Utils
+          Welcome to <a href="https://loqo71la.dev">loqo71la</a>&apos;s Web Utils
         </h1>
       </main>
     </div>
